5368e3224fd242d33e002e597eb89fe3c0d3aaa6
Carlo Carpio <carlo@omh.sg>
2018-09-11 16:10:27 +0800

    Added app and tests.

---
+++ tests/unit/models/item_test2.py
@@ -0,0 +1,19 @@
+from models.item import ItemModel
+from models.store import StoreModel
+from tests.base_test import BaseTest
+
+
+class ItemTest(BaseTest):
+    def test_crud(self):
+        with self.app_context():
+            StoreModel('test').save_to_db()
+            item = ItemModel('test', 19.99, 1)
+
+            self.assertIsNone(ItemModel.find_by_name('test'),
+                              'Found an item with name{}, but expected not to.'.format(item.name))
+            item.save_to_db()
+            self.assertIsNotNone(ItemModel.find_by_name('test'),
+                                 'Found no item with name{}, but expected to.'.format(item.name))
+            item.delete_from_db()
+            self.assertIsNone(ItemModel.find_by_name('test'),
+                              'Found an item with name{}, but expected not to.'.format(item.name))
